da555857961a9791d68c0b1828345a8a731a2a78
Pallavi <pallavilpmt1995@gmail.com>
2024-12-16 02:29:25 +0530

    hero Section done

--- CodeWaveAI/src/App.tsx
+++ CodeWaveAI/src/App.tsx
@@ -1,9 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import Header from "./components/Header.tsx";
+import Hero from './components/Hero.tsx';
 import Button from "./components/Button.tsx";
-
-
 import ButtonSvg from "./assets/svg/ButtonSvg.tsx";
 
 
@@ -12,11 +11,8 @@ function App() {
     <>
     <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
     <Header />
+    <Hero />
     </div>
-
-
-
-    
     </>
   )
 }
---
+++ CodeWaveAI/src/components/Generating.tsx
@@ -0,0 +1,17 @@
+import loading from "../assets/load.png";
+
+//@ts-ignore
+const Generating = ({ className }) => {
+  return (
+    <div
+      className={`flex items-center h-[3.5rem] px-6 bg-n-8/90 rounded-[1.7rem] ${
+        className || ""
+      } text-base`}
+    >
+      <img className="w-9 h-9 mr-4" src={loading} alt="Loading" />
+      AI is generating . . . .
+    </div>
+  );
+};
+
+export default Generating;
