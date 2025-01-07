5925d19a843fa5f3836c1fffd26445c32fb75dbc
Pallavi <pallavilpmt1995@gmail.com>
2025-01-07 15:54:41 +0530

    backend upd

--- Backend/src/routes/project.routes.ts
+++ Backend/src/routes/project.routes.ts
@@ -31,8 +31,6 @@ router.post('/create', authMiddleware, async(req, res)=>{
     } catch (error) {
         throw new Error('An error occured while creating project');
     }
-
-
 })
 
 //@ts-ignore
@@ -62,11 +60,86 @@ router.get('/all',authMiddleware, async(req, res)=>{
     
 })
 
-router.put('/add-user',authMiddleware, async(req, res)=>{
-    
-})
+
+//@ts-ignore
+router.put('/add-user', authMiddleware, async (req, res) => {
+    try {
+        const { projectId, username } = req.body;
+
+        if (!projectId || !username) {
+            return res.status(400).json({ error: 'Invalid input' });
+        }
+
+        // Find the logged-in user
+        //@ts-ignore
+        const loggedInUser = await UserModel.findById(req.userId);
+        if (!loggedInUser) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        // Find the project and check if the logged-in user is the creator or part of the project
+        const project = await ProjectModel.findOne({
+            _id: projectId,
+            $or: [
+                { userId: loggedInUser._id }, // Project creator
+                { users: loggedInUser._id }   // Existing project member
+            ]
+        });
+
+        if (!project) {
+            return res.status(403).json({ error: 'User is not authorized to add users to this project' });
+        }
+
+        // Find the user to be added by username
+        const userToAdd = await UserModel.findOne({ username });
+        if (!userToAdd) {
+            return res.status(404).json({ error: 'User to be added not found' });
+        }
+
+        // Add the user to the project
+        const updatedProject = await ProjectModel.findByIdAndUpdate(
+            projectId,
+            {
+                $addToSet: {
+                    users: userToAdd._id
+                }
+            },
+            { new: true }
+        );
+
+        if (!updatedProject) {
+            return res.status(404).json({ error: 'Project not found' });
+        }
+
+        res.status(200).json({
+            message: 'User added successfully',
+            project: updatedProject
+        });
+
+    } catch (err) {
+        console.log(err);
+        //@ts-ignore
+        res.status(500).json({ error: err.message });
+    }
+});
+
+
 
 router.get('/get-project/:projectId',authMiddleware, async(req, res)=>{
+    const { projectId } = req.params;
+
+    try {
+
+        const project = await ProjectModel.findOne({
+            _id: projectId
+        }).populate('users')
+
+    } catch (err) {
+        console.log(err)
+        //@ts-ignore
+        res.status(400).json({ error: err.message })
+    }
+
     
 })
 
