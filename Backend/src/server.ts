3802e19cb8696bb1b1babfcd0415fd943eb1254a
Pallavi <pallavilpmt1995@gmail.com>
2025-03-03 14:23:41 +0530

    BE cors update

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -15,7 +15,7 @@ const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
     cors: {
         origin: '*',
-        methods: ['GET', 'POST'],
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
     }
 });
 
