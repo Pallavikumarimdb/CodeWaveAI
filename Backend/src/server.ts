3683949939bd5b87b6deb60ac09274de6cf28590
Pallavi <pallavilpmt1995@gmail.com>
2025-03-04 00:16:16 +0530

    attempt 1

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -9,6 +9,15 @@ const app=express();
 app.use(express.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+
 
 app.use(express.urlencoded({ extended: true }));
 
